cf7cc6911765bf28fc52c5efd493c6e1804694f1
KushwanthBabu <kushwanthchowdary990@gmail.com>
2026-02-12 13:42:34 +0530

    Commit New Files

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,9 @@ app.use("/api/players", require("./routes/playerRoutes"));
 
 startScheduler();
 
-app.listen(process.env.PORT, function () {
-  console.log("Server running on port", process.env.PORT);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function () {
+  console.log("Server running on port", PORT);
 });
+
